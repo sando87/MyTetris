29da445a0cb829691690776ece2245c05c7d4f89
sando87 <sando87@gmail.com>
2019-05-06 08:10:53 +0900

    working on moving blocks

---
+++ Assets/Script/CSBackground.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSBackground : MonoBehaviour
+{
+    public GameObject mBlockObject;
+    public Material mMaterial;
+    public int mCountX;
+    public int mCountY;
+
+    public static float mBlockSize = 0.25f;
+
+    private byte[,] mBlockArray = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InitBackground();
+        InitBlocks();
+    }
+
+    float timeAcc = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timeAcc += Time.deltaTime;
+        if(timeAcc > 3)
+        {
+            CreateNewBlock();
+            timeAcc = 0;
+        }
+    }
+
+    void InitBlocks()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> indicies = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        mBlockArray = new byte[mCountX, mCountY];
+        for(int y = 0; y<mCountY; ++y)
+        {
+            for (int x = 0; x < mCountX; ++x)
+            {
+                vertices.Add(new Vector3((mBlockSize * (x + 0)), (mBlockSize * (y + 0)), 0));
+                vertices.Add(new Vector3((mBlockSize * (x + 0)), (mBlockSize * (y + 1)), 0));
+                vertices.Add(new Vector3((mBlockSize * (x + 1)), (mBlockSize * (y + 0)), 0));
+                vertices.Add(new Vector3((mBlockSize * (x + 1)), (mBlockSize * (y + 1)), 0));
+
+                int cnt = indicies.Count;
+                indicies.AddRange( new int[] {
+                    cnt+0, cnt+1, cnt+2, cnt+1, cnt+3, cnt+2,
+                });
+
+                int rand = Random.Range(0, 7);
+                uvs.Add(new Vector2(0.125f * (rand + 0), 0));
+                uvs.Add(new Vector2(0.125f * (rand + 0), 1));
+                uvs.Add(new Vector2(0.125f * (rand + 1), 0));
+                uvs.Add(new Vector2(0.125f * (rand + 1), 1));
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = indicies.ToArray();
+        mesh.uv = uvs.ToArray();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().material = mMaterial;
+    }
+
+    void InitBackground()
+    {
+        mBlockArray = new byte[mCountX, mCountY];
+
+        Mesh mesh = new Mesh();
+        float MaxX = mBlockSize * mCountX;
+        float MaxY = mBlockSize * mCountY;
+        mesh.vertices = new Vector3[4]
+        {
+            new Vector3(0,      0,      0),
+            new Vector3(0,      MaxY,   0),
+            new Vector3(MaxX,   0,      0),
+            new Vector3(MaxX,   MaxY,   0)
+        };
+        mesh.triangles = new int[] 
+        {
+            0, 1, 2, 1, 3, 2,
+        };
+        mesh.uv = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+        };
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().material = mMaterial;
+    }
+
+    void CreateNewBlock()
+    {
+        int rand = Random.Range(0, 7) + 1;
+        CSBlock.BlockType type = (CSBlock.BlockType)rand;
+        GameObject obj = Instantiate(mBlockObject, new Vector3(rand, 0, 0), Quaternion.identity) as GameObject;
+        obj.GetComponent<CSBlock>().mType = type;
+    }
+}
--- Assets/Script/CSBlock.cs
+++ Assets/Script/CSBlock.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class CSBlock : MonoBehaviour
 {
-    
-    enum BlockType
+    public Material mMaterialBlock;
+    public BlockType mType;
+
+    public enum BlockType
     {
         NONE, I, J, L, O, S, T, Z
     };
-    BlockType mType = BlockType.NONE;
     byte[,] mShape = new byte[4,4];
     int mState = 0;
     int mMaxState = 0;
 
-    CSBlock(BlockType _type)
+    void InitBlock()
     {
-        mType = _type;
         mState = 0;
-        switch(mType)
+        switch (mType)
         {
             case BlockType.I: mMaxState = 2; break;
             case BlockType.J: mMaxState = 4; break;
@@ -29,10 +29,11 @@ public class CSBlock : MonoBehaviour
             case BlockType.Z: mMaxState = 2; break;
             default: mMaxState = 1; break;
         }
-        UpdateShape();
+        UpdateShapeArray();
+        InitMesh();
+        GetComponent<MeshRenderer>().material = mMaterialBlock;
     }
-
-    void UpdateShape()
+    void UpdateShapeArray()
     {
         switch (mType)
         {
@@ -79,18 +80,18 @@ public class CSBlock : MonoBehaviour
                 {
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
-                        { 1, 1, 0, 0},
-                        { 1, 0, 0, 0},
-                        { 1, 0, 0, 0}
+                        { 0, 1, 1, 0},
+                        { 0, 1, 0, 0},
+                        { 0, 1, 0, 0}
                     };
                 }
                 else
                 {
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
+                        { 0, 0, 0, 0},
                         { 1, 1, 1, 0},
-                        { 0, 0, 1, 0},
-                        { 0, 0, 0, 0}
+                        { 0, 0, 1, 0}
                     };
                 }
                 break;
@@ -99,18 +100,18 @@ public class CSBlock : MonoBehaviour
                 {
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
-                        { 1, 0, 0, 0},
-                        { 1, 0, 0, 0},
-                        { 1, 1, 0, 0}
+                        { 0, 1, 0, 0},
+                        { 0, 1, 0, 0},
+                        { 0, 1, 1, 0}
                     };
                 }
                 else if (mState == 1)
                 {
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
+                        { 0, 0, 0, 0},
                         { 1, 1, 1, 0},
-                        { 1, 0, 0, 0},
-                        { 0, 0, 0, 0}
+                        { 1, 0, 0, 0}
                     };
                 }
                 else if (mState == 2)
@@ -146,8 +147,8 @@ public class CSBlock : MonoBehaviour
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
                         { 0, 1, 1, 0},
-                        { 0, 1, 0, 0},
-                        { 1, 1, 0, 0}
+                        { 1, 1, 0, 0},
+                        { 0, 0, 0, 0}
                     };
                 }
                 else
@@ -155,8 +156,8 @@ public class CSBlock : MonoBehaviour
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
                         { 1, 0, 0, 0},
-                        { 1, 1, 1, 0},
-                        { 0, 0, 1, 0}
+                        { 1, 1, 0, 0},
+                        { 0, 1, 0, 0}
                     };
                 }
                 break;
@@ -204,8 +205,8 @@ public class CSBlock : MonoBehaviour
                     mShape = new byte[4, 4] {
                         { 0, 0, 0, 0},
                         { 1, 1, 0, 0},
-                        { 0, 1, 0, 0},
-                        { 0, 1, 1, 0}
+                        { 0, 1, 1, 0},
+                        { 0, 0, 0, 0}
                     };
                 }
                 else
@@ -223,14 +224,153 @@ public class CSBlock : MonoBehaviour
     void NextShape()
     {
         mState = (mState + 1) % mMaxState;
-        UpdateShape();
+        UpdateShapeArray();
     }
+    void InitMesh()
+    {
+        Mesh mesh = new Mesh();
 
+        Vector2Int[] shapeArray;
+        switch(mType)
+        {
+            case BlockType.I:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(0, 2),
+                    new Vector2Int(0, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(0, -1),
+                };
+                break;
+            case BlockType.J:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(0, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(0, -1),
+                    new Vector2Int(-1, -1),
+                };
+                break;
+            case BlockType.L:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(0, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(0, -1),
+                    new Vector2Int(1, -1),
+                };
+                break;
+            case BlockType.O:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(0, 1),
+                    new Vector2Int(1, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(1, 0),
+                };
+                break;
+            case BlockType.S:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(0, 1),
+                    new Vector2Int(1, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(-1, 0),
+                };
+                break;
+            case BlockType.T:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(-1, 0),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(1, 0),
+                    new Vector2Int(0, -1),
+                };
+                break;
+            case BlockType.Z:
+                shapeArray = new Vector2Int[4]
+                {
+                    new Vector2Int(-1, 1),
+                    new Vector2Int(0, 1),
+                    new Vector2Int(0, 0),
+                    new Vector2Int(1, 0),
+                };
+                break;
+            default:
+                return;
+        }
+        Vector3[] verticies = NewVerticies(shapeArray);
+        mesh.vertices = verticies;
+
+        mesh.triangles = new int[] {
+            0, 1, 2, 1, 3, 2,
+            4, 5, 6, 5, 7, 6,
+            8, 9, 10, 9, 11, 10,
+            12, 13, 14, 13, 15, 14,
+        };
+        mesh.uv = NewUVVectors(mType);
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+    Vector2[] NewUVVectors(BlockType _type)
+    {
+        int uvIndex = 0;
+        float step = 0.125f;
+        switch(_type)
+        {
+            case BlockType.I: uvIndex = 0; break;
+            case BlockType.J: uvIndex = 1; break;
+            case BlockType.L: uvIndex = 2; break;
+            case BlockType.O: uvIndex = 3; break;
+            case BlockType.S: uvIndex = 4; break;
+            case BlockType.T: uvIndex = 5; break;
+            case BlockType.Z: uvIndex = 6; break;
+            default: break;
+        }
+        Vector2[] ret = new Vector2[]
+        {
+            new Vector2(uvIndex * step,         0),
+            new Vector2(uvIndex * step,         1),
+            new Vector2((uvIndex + 1) * step,   0),
+            new Vector2((uvIndex + 1) * step,   1),
+            new Vector2(uvIndex * step,         0),
+            new Vector2(uvIndex * step,         1),
+            new Vector2((uvIndex + 1) * step,   0),
+            new Vector2((uvIndex + 1) * step,   1),
+            new Vector2(uvIndex * step,         0),
+            new Vector2(uvIndex * step,         1),
+            new Vector2((uvIndex + 1) * step,   0),
+            new Vector2((uvIndex + 1) * step,   1),
+            new Vector2(uvIndex * step,         0),
+            new Vector2(uvIndex * step,         1),
+            new Vector2((uvIndex + 1) * step,   0),
+            new Vector2((uvIndex + 1) * step,   1),
+        };
+        return ret;
+    }
+    Vector3[] NewVerticies(Vector2Int[] _array)
+    {
+        float blockSize = CSBackground.mBlockSize;
+        float halfSize = blockSize * 0.5f;
+        int blockCount = _array.Length;
+        Vector3[] verticies = new Vector3[blockCount * 4];
+        for(int i = 0; i < blockCount; ++i)
+        {
+            int PosX = _array[i].x;
+            int PosY = _array[i].y;
+            verticies[i * 4 + 0] = new Vector3((blockSize * PosX) - halfSize, (blockSize * PosY) - halfSize , 0);
+            verticies[i * 4 + 1] = new Vector3((blockSize * PosX) - halfSize, (blockSize * PosY) + halfSize , 0);
+            verticies[i * 4 + 2] = new Vector3((blockSize * PosX) + halfSize, (blockSize * PosY) - halfSize , 0);
+            verticies[i * 4 + 3] = new Vector3((blockSize * PosX) + halfSize, (blockSize * PosY) + halfSize , 0);
+        }
+        return verticies;
+    }
     
 
     // Start is called before the first frame update
     void Start()
     {
+        InitBlock();
     }
 
     // Update is called once per frame
